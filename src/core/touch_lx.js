b0e92090441e0081b5c47377c8c1d90fe24ddb84
lxcuso4 <lxcuso4@aliyun.com>
2016-10-18 11:57:28 +0800

    touch 事件

---
+++ src/core/touch_lx.js
@@ -0,0 +1,67 @@
+/**
+ * Created by liux on 2016/10/17.
+ */
+;!function ($) {
+    "use strict";
+    var doubleTapEl,singleTimer,longTapTimer;
+    const TapD = 7,SwipeD = 10, DoubleTapDelay = 300,LongTapDelay = 500,TapTimeout=1000, TouchTimeout=2000;
+    function swipeDirection(x1, x2, y1, y2) {
+        return Math.abs(x1 - x2) >=
+        Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down')
+    }
+    $(document).ready(function () {
+        var el,startTime,endTime,startX,startY,endX,endY;
+        function Range(){
+            return  Math.max(Math.abs(endX-startX),Math.abs(endY-startY));
+        }
+        function touchType() {
+            var delay = endTime - startTime,
+                range = Range();
+            clearTimeout(longTapTimer);
+            if(delay>TouchTimeout)return;//按住的时间超过2s取消所有事件
+            if(range>SwipeD){
+                el.trigger('swipe');
+                el.trigger('swipe' + swipeDirection(startX, endX, startY, endY));
+                return
+            }else if(range<TapD && delay<TapTimeout){//按住超过1s取消所有tap类型的事件
+                el.trigger('tap');
+                if(delay < DoubleTapDelay){
+                    if(el[0] === doubleTapEl){
+                        doubleTapEl = null;
+                        el.trigger('doubleTap');
+                        clearTimeout(singleTimer);
+                    }else {
+                        doubleTapEl = el[0];
+                        singleTimer = setTimeout(function () {
+                            el.trigger('singleTap')
+                        },DoubleTapDelay);
+                    }
+                }
+            }
+        }
+        $(document)
+            .on('touchstart',function (e) {
+                el = $(event.target);
+                startTime = Date.now();
+                startX = endX = e.touches[0].pageX;
+                startY = endY = e.touches[0].pageY;
+                longTapTimer = setTimeout(function () {
+                    if(Range()<TapD) el.trigger('longTap')
+                },LongTapDelay);
+            })
+            .on('touchmove',function (e) {
+                if(!el||e.target !== el[0])return;
+                endX = e.touches[0].pageX;
+                endY = e.touches[0].pageY;
+            })
+            .on('touchend',function (e) {
+                if(!el||e.target !== el[0])return;
+                endTime = Date.now();
+                touchType();
+            })
+            .on('touchcanle',function () {
+                if(!el||e.target !== el[0])return;
+                clearTimeout(longTapTimer)
+            });
+    });
+}($);
